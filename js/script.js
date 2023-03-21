3963e1ef89cea1ed7f753fc181b6fd440bce800a
GuiMartinsDev <guigalego96@gmail.com>
2023-03-20 23:22:01 -0300

    exibindo-detalhes-curso-e-2-img

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const botoesList = document.querySelectorAll(".detalhes-btn");
+console.log(botoesList);
+const botoesArray = [...botoesList];
+console.log(botoesArray);
 
+function mostrarDetalhes(event) {
+  const botao = event.currentTarget;
+  botao.classList.toggle("ativo");
+}
+
+botoesArray.forEach((botao) => {
+  botao.addEventListener("click", mostrarDetalhes);
+});

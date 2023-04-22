04077a2120789ae6da66e0586275f4793cd97774
GuiMartinsDev <guigalego96@gmail.com>
2023-04-22 01:39:27 -0300

    muitas-alteracoes-header-fixo-scroll-nos-cursos

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ const botoesDetails = document.querySelectorAll(".detalhes-btn");
 
 function mostrarDetalhes(event) {
   const botao = event.currentTarget;
+  botao.classList.toggle("active");
   const details = botao.nextElementSibling;
-  details.classList.toggle('ativo');
-};
+  details.classList.toggle("ativo");
+}
 
 botoesDetails.forEach((botao) => {
   botao.addEventListener("click", mostrarDetalhes);

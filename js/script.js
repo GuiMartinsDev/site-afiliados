63a2a2a4a1da41bdf2d582a5eefaac749ccc7ab0
GuiMartinsDev <guigalego96@gmail.com>
2023-05-02 00:32:24 -0300

    add-faq-animacoes-inicio-do-footer

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ function mostrarDetalhes(event) {
 botoesDetails.forEach((botao) => {
   botao.addEventListener("click", mostrarDetalhes);
 });
+
+const faqLista = document.querySelectorAll(".js-accordion dt");
+const ativo = "ativo";
+
+if (faqLista.length) {
+  faqLista[0].classList.add(ativo);
+  faqLista[0].nextElementSibling.classList.add(ativo);
+}
+
+function addClassAtivo() {
+  this.classList.toggle(ativo);
+  this.nextElementSibling.classList.toggle(ativo);
+}
+
+faqLista.forEach((dt) => {
+  dt.addEventListener("click", addClassAtivo);
+});

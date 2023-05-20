50377a40baf859ff21d7ea09e5d475ddbdf69146
GuiMartinsDev <guigalego96@gmail.com>
2023-05-20 00:14:59 -0300

    muitas-mudancas-add-banner-header-animado

--- js/script.js
+++ js/script.js
@@ -29,12 +29,23 @@ faqLista.forEach((dt) => {
 });
 
 function initAnimacaoScroll() {
+  const header = document.querySelector(".header-bg");
+  const firstSection = document.querySelector(".introducao");
   const sections = document.querySelectorAll(".js-scroll");
+  const h2Footer = document.querySelector(".contato-h2");
   const footer = document.querySelector(".footer");
-  if (sections.length && footer) {
+  if (sections.length && footer && header) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
+      const firstSectionTop = firstSection.getBoundingClientRect().top;
+      console.log(firstSectionTop);
+      const headerSolid = firstSectionTop - 10 < 0;
+      if (headerSolid) {
+        header.classList.add("solid");
+      } else {
+        header.classList.remove("solid");
+      }
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = sectionTop - windowMetade < 0;
@@ -44,11 +55,13 @@ function initAnimacaoScroll() {
           section.classList.remove("ativo");
         }
       });
-      const footerTop = footer.getBoundingClientRect().top;
-      const isFooterVisible = footerTop - windowMetade < 130;
+      const h2FooterTop = h2Footer.getBoundingClientRect().top;
+      const isFooterVisible = h2FooterTop - windowMetade < 200;
       if (isFooterVisible) {
+        h2Footer.classList.add("ativo");
         footer.classList.add("ativo");
       } else {
+        h2Footer.classList.remove("ativo");
         footer.classList.remove("ativo");
       }
     }

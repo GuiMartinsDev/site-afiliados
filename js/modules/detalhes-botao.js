f44d3ac679bc5bd4252d9eeaa17af4e69546f893
GuiMartinsDev <guigalego96@gmail.com>
2023-06-29 01:10:12 -0300

    modal-detalhes

--- js/modules/detalhes-botao.js
+++ js/modules/detalhes-botao.js
@@ -1,19 +1,40 @@
 export default function initDetalhesBtn() {
   const botoesDetails = document.querySelectorAll(".detalhes-btn");
+  const modalContainer = document.querySelector("[data-modal='container']");
+  const modalDetalhes = document.querySelector(
+    "[data-modal='container'] .modal-detalhes"
+  );
+  const btnFechar = document.querySelector("[data-modal='fechar']");
 
   function mostrarDetalhes(event) {
     if (event.type === "touchstart") {
       event.preventDefault();
     }
     const botao = event.currentTarget;
-    botao.classList.toggle("active");
 
-    const details = botao.nextElementSibling;
-    details.classList.toggle("ativo");
+    const details = botao.nextElementSibling.innerHTML;
+
+    const txt = document.createElement("p");
+    txt.innerHTML = details;
+    txt.dataset.detalhes = "paragrafo";
+    txt.classList.add("font-p");
+    modalDetalhes.appendChild(txt);
+    modalContainer.classList.add("ativo");
+  }
+
+  function fecharDetalhes(event) {
+    if (event.type === "touchstart") {
+      event.preventDefault();
+    }
+    modalContainer.classList.remove("ativo");
+    modalDetalhes.innerHTML = "";
   }
 
   botoesDetails.forEach((botao) => {
-    botao.addEventListener("click", mostrarDetalhes);
-    botao.addEventListener("touchstart", mostrarDetalhes);
+    ["click", "touchstart"].forEach((userEvent) => {
+      botao.addEventListener(userEvent, mostrarDetalhes);
+    });
   });
+  btnFechar.addEventListener("click", fecharDetalhes);
+  btnFechar.addEventListener("touchstart", fecharDetalhes);
 }
